9fbb14415aeb84d29c4cd306711fe2b05fcef540
sokollya <ordinals1712@gmail.com>
2022-07-04 13:14:01 +0200

    Search Engine

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Dictionary />
+    </div>
+  );
 }
 
 export default App;
