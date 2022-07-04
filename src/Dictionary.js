6a82e8b1e7f5449aadb700f54912de9c257230fd
sokollya <ordinals1712@gmail.com>
2022-07-04 13:16:20 +0200

    Search Engine

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,6 +4,7 @@ export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   function search(event) {
     event.preventDefault();
+    alert(`Searching for ${keyword}`);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
