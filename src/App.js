f6554deade9efc311e1ec1f2810d2107ea715bd2
sokollya <ordinals1712@gmail.com>
2022-07-05 20:18:09 +0200

    Photos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import Dictionary from "./Dictionary";
+import dictionary from "./dictionary.jpg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <img src={dictionary} className="img-fluid" alt="dictionary" />
       <Dictionary />
+      <footer className="text-center">
+        <small>
+          This project was coded by Yana Sokol and is{" "}
+          <a
+            href="https://github.com/sokollya/dictionary-react-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open sourced on Git Hub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://neon-crumble-f62cbb.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+          .
+        </small>
+      </footer>
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,30 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos.js";
 import "./Dictionary.css";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("time");
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
   function handleResponse(response) {
     setResults(response.data[0]);
   }
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000018f03e00b6877418f8b4b391d1ee7b07f";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
   function handleSubmit(event) {
     event.preventDefault();
@@ -45,6 +57,7 @@ export default function Dictionary() {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
